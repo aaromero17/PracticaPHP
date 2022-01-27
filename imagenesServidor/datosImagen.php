518e919b3b0e490938195286b717790033e1537f
aaromero <aaromero17@gmail.com>
2022-01-27 15:55:04 -0400

    envio de direccion de correo electronico

---
+++ imagenesServidor/datosImagen.php
@@ -0,0 +1,7 @@
+<?php
+    //recibimos los datos de la imagen
+    $nombre_imagen=$_FILES["imagen"]["name"];
+    $nombre_tipo=$_FILES["imagen"]["type"];
+    $nombre_tamano=$_FILES["imagen"]["size"];
+
+?>
